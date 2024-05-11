32292d2233b7eb9d5dc72b6ea961f70e71982b4f
ediberto <coronelvediberto@gmail.com>
2024-05-11 00:39:03 -0400

    agregando cambios

--- Sitio Web Connect Salud/ProgramadorWeb2023/ConnetSalud/src/app/app.module.ts
+++ Sitio Web Connect Salud/ProgramadorWeb2023/ConnetSalud/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { app_routing } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -15,56 +15,48 @@ import { RegisterComponent } from './components/register/register.component';
 import { MonitoreodepesoComponent } from './components/monitoreodepeso/monitoreodepeso.component';
 import { AuthGuard } from './components/guard/auth.guard';
 import { CarritoComponent } from './components/carrito/carrito.component';
-import { ListCitasComponent } from './components/list-citas/list-citas.component';
 import { EditCitasComponent } from './components/edit-citas/edit-citas.component';
-import { TurnosComponent } from './components/turnos/turnos.component';
-import { PerfilComponent } from './components/perfil/perfil.component';
-import { DataService } from './components/services/data.service';
-import { CartService } from './components/services/cart.service';
+import { ListCitasComponent } from './components/list-citas/list-citas.component';
+import { TurnosComponent } from './components/index.paginas';
 import { ListUsersComponent } from './components/list-users/list-users.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
 import { EditUsersComponent } from './components/edit-users/edit-users.component';
 import { AuthService } from './components/services/auth.service';
-import {AuthStateService} from './components/services/auth-state.service'
+import { AuthStateService } from './components/services/auth-state.service';
 import { UserService } from './components/services/user.service';
-
-
-
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    InicioComponent,
-    ContactoComponent,
-    PlanesComponent,
-    RecetasComponent,
-    LoginComponent,
-    RegisterComponent,
-    MonitoreodepesoComponent,
-    CarritoComponent,
-    ListCitasComponent,
-    PerfilComponent,
-    EditCitasComponent,
-    TurnosComponent,
-    ListUsersComponent,
-    EditUsersComponent,
-   ],
-  imports: [
-    BrowserModule,
-    app_routing,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-  ],
-  providers: [
-    AuthGuard,
-    DataService,
-    CartService,
-    AuthService,
-    AuthStateService,
-    UserService,
-  ],
-  bootstrap: [AppComponent]
+declarations: [
+AppComponent,
+HeaderComponent,
+FooterComponent,
+InicioComponent,
+ContactoComponent,
+PlanesComponent,
+RecetasComponent,
+LoginComponent,
+RegisterComponent,
+MonitoreodepesoComponent,
+CarritoComponent,
+EditCitasComponent,
+ListCitasComponent,
+TurnosComponent,
+ListUsersComponent,
+PerfilComponent,
+EditUsersComponent,
+],
+imports: [
+BrowserModule,
+app_routing,
+FormsModule,
+ReactiveFormsModule,
+HttpClientModule,
+],
+providers: [
+AuthGuard,
+AuthService,
+AuthStateService,
+UserService,
+],
+bootstrap: [AppComponent]
 })
 export class AppModule { }
--- Sitio Web Connect Salud/ProgramadorWeb2023/ConnetSalud/src/app/components/index.paginas.ts
+++ Sitio Web Connect Salud/ProgramadorWeb2023/ConnetSalud/src/app/components/index.paginas.ts
@@ -10,9 +10,6 @@ export {TurnosComponent} from "./turnos/turnos.component";
 export {CarritoComponent} from "./carrito/carrito.component";
 export {ListCitasComponent} from "./list-citas/list-citas.component";
 export {EditCitasComponent} from "./edit-citas/edit-citas.component";
-export {PerfilComponent} from "./perfil/perfil.component";
 export {ListUsersComponent} from "./list-users/list-users.component";
 export {EditUsersComponent} from "./edit-users/edit-users.component";
-
-
-
+export {PerfilComponent} from "./perfil/perfil.component";
