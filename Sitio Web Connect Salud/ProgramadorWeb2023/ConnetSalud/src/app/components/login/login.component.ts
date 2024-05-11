e61e85fa805ef373475d2a06d984c6dd11730e4d
samirasanchez1 <samira.sanchez99@outlook.com>
2024-05-10 23:58:38 -0300

    se agregan cambios en componente login

--- Sitio Web Connect Salud/ProgramadorWeb2023/ConnetSalud/src/app/components/login/login.component.ts
+++ Sitio Web Connect Salud/ProgramadorWeb2023/ConnetSalud/src/app/components/login/login.component.ts
@@ -7,39 +7,37 @@ import { first } from 'rxjs/operators'
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-  
 })
 
 export class LoginComponent implements OnInit {
-  
+
   myform: FormGroup;
 
-  constructor( private authService: AuthService ) {}
-
+  constructor(private authService: AuthService) { }
+ 
   ngOnInit(): void {
     this.myform = new FormGroup({
       username: new FormControl(''),
       password: new FormControl(''),
     });
-    };
-
+  };
   get f() {
     return this.myform.controls;
   }
-
+  
+ 
   onSubmit() {
     console.log(this.myform.value);
-    this.authService.login( this.f['username'].value, this.f['password'].value )
-    .pipe(first())
-    .subscribe(
-      data => {
-        console.log(data);
-      },
-      error => {
-        console.log(error);
-        alert('No tienes acceso')
-      }
-    )
+    this.authService.login(this.f['username'].value, this.f['password'].value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          console.log(data);
+        },
+        error => {
+          console.log(error);
+          alert('No tienes acceso')
+        }
+      )
   }
-
-  }
+}
