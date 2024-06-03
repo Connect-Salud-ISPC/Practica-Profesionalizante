bac4b07cc8fa47abbbd5275ded0e9d3bc3f436f7
samirasanchez1 <samira.sanchez99@outlook.com>
2024-06-02 22:57:30 -0300

    se agrega funcionalidad de java contacto

--- App Movil Turnero Connect Salud/app/src/main/java/com/example/connectsalud/Contacto.java
+++ App Movil Turnero Connect Salud/app/src/main/java/com/example/connectsalud/Contacto.java
@@ -1,24 +1,49 @@
 package com.example.connectsalud;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Contacto extends AppCompatActivity {
+public class ContactoActivity extends AppCompatActivity {
+
+    private EditText editTextName;
+    private EditText editTextEmail;
+    private EditText editTextSubject;
+    private EditText editTextMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_contacto);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        editTextName = findViewById(R.id.editTextName);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextSubject = findViewById(R.id.editTextSubject);
+        editTextMessage = findViewById(R.id.editTextMessage);
+    }
+
+    public void sendContactMessage(View view) {
+        String name = editTextName.getText().toString().trim();
+        String email = editTextEmail.getText().toString().trim();
+        String subject = editTextSubject.getText().toString().trim();
+        String message = editTextMessage.getText().toString().trim();
+
+        if (name.isEmpty() || email.isEmpty() || subject.isEmpty() || message.isEmpty()) {
+            Toast.makeText(this, "Por favor, complete todos los campos", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // Mostrar mensaje de éxito y limpiar los campos
+        Toast.makeText(this, "Mensaje enviado", Toast.LENGTH_SHORT).show();
+        limpiarCampos();
+    }
+
+    private void limpiarCampos() {
+        editTextName.setText("");
+        editTextEmail.setText("");
+        editTextSubject.setText("");
+        editTextMessage.setText("");
     }
 }
